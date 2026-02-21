e0f16d6760d581faf424b0ea66874482224dd2ea
luizstsantos <luiz.st.santos@gmail.com>
2026-02-21 18:39:35 -0300

    Adiciona script de build e gitignore

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+VERSION="v1.0.0"
+NAME="chronoscast-linux-amd64"
+OUTPUT="${NAME}-${VERSION}"
+
+echo "🔨 Compilando o Chronoscast..."
+go build -ldflags="-s -w" -o $OUTPUT cmd/server/main.go
+
+echo "--------------------------------------------------"
+echo "✅ Sucesso! build completa $OUTPUT."
